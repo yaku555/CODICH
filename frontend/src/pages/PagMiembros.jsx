753944e93574061f1a4c263962d8645edb21dc69
panxo <franciscovillalobos025@gmail.com>
2026-05-23 22:47:33 -0400

    funciona postulacion, funciona acceso por roles (falta mejorar) y se muestra a medias la info del usuario

--- frontend/src/pages/PagMiembros.jsx
+++ frontend/src/pages/PagMiembros.jsx
@@ -1,10 +1,15 @@
+import { useUsuario } from "../context/usuario.context";
+
 function PagMiembros() {
+  const { usuario, logout } = useUsuario();
+
   return (
     <main className="page">
       <h1>Tu Información</h1>
-      <p>
-          prueba
-      </p>
+      <p>Nombre: {usuario?.nombre} {usuario?.apellido}</p>
+      <p>Email: {usuario?.email}</p>
+      <p>Rol: {usuario?.rol}</p>
+      <button onClick={logout}>Cerrar sesión</button>
     </main>
   );
 }
