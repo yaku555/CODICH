e8bc3d98ec0bfefcf29bb9887f04795dfbcde938
panxo <franciscovillalobos025@gmail.com>
2026-05-26 19:24:04 -0400

    feat: creacion de la rama

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL del backend test
 const instance = axios.create({
-  baseURL: 'http://localhost:6767/api', 
+  baseURL: 'http://localhost:4000/api', 
 
 
 });
